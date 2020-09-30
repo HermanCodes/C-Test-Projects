1cb5db24b71064f8a4fd6eb18338176a8bab2d63
Hermanator09 <63057616+Hermanator09@users.noreply.github.com>
2020-09-30 15:54:26 +0100

    Add files via upload

---
+++ CoinCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //writing and gathering the initial information
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Please enter an amount in cents to convert to coins:");
+            string totalAsString = Console.ReadLine();
+            double total = Math.Floor(Convert.ToDouble(totalAsString));
+            Console.WriteLine($"{total} cents is equal to...");
+
+            //defining coin values
+            int quaterValue = 25;
+            int dimeValue = 10;
+            int nickleValue = 5;
+
+            //calculate the change
+            double quaterCoins = Math.Floor(total / quaterValue);
+            double remainder = total % quaterValue;
+
+            double dimeCoins = Math.Floor(remainder / dimeValue);
+            remainder %= dimeValue;
+            /*Console.WriteLine(silverCoins);
+            Console.WriteLine(remainder);*/
+
+            double nickleCoins = Math.Floor(remainder / nickleValue);
+            remainder %= nickleValue;
+
+            //print results of all coins
+            Console.WriteLine($"Quater coins: {quaterCoins}");
+            Console.WriteLine($"Dime coins: {dimeCoins}");
+            Console.WriteLine($"Nickle coins: {nickleCoins}");
+            Console.WriteLine($"Penny coins: {remainder}");
+        }
+    }
+}
---
+++ Compound Interest Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * C# Program to Calculate Compound Interest
+ */
+using System;
+namespace compund
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount;
+            double interest;
+            int years;
+            double balance = 0;
+
+            Console.WriteLine("Enter the Amount");
+            amount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the Interest Rate");
+            interest = Convert.ToDouble(Console.ReadLine()) / 100;
+
+            Console.WriteLine("Enter number of Years");
+            years = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = years; i >= 0; i--)
+            {
+                balance = amount * Math.Pow((1 + interest), years);
+
+
+            }
+
+            {
+                Console.WriteLine("Total Amount Balance is: ${0}", balance);
+            }
+            Console.ReadLine();
+        }
+    }
+}
